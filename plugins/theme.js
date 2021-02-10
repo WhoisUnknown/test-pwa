27c0a761512575ab72f20d0e46111e8c77ee5e02
Maxim <rak.2304@mail.ru>
2021-02-10 12:16:46 +0500

    add css vars on client

--- plugins/theme.js
+++ plugins/theme.js
@@ -17,16 +17,12 @@ export default ({ app, $cookies }, inject) => {
       console.warn(`Тема "${theme}" не найдена`);
     }
   }
-  // добавляем переменные только на сервере
-  if (process.server) {
-  // добавляем все переменные из config.cssVars в :root страницы
-    const stringStyle = Object.keys(resultConfig.cssVars).reduce((prevVal, key) => {
-      prevVal += `--${key}: ${resultConfig.cssVars[key]};\n`;
-      return prevVal;
-    }, '');
-    app.head.style.push(
-      { innerHTML: `:root{\n ${stringStyle}}`, charset: 'utf-8' }
-    );
-  }
+  const stringStyle = Object.keys(resultConfig.cssVars).reduce((prevVal, key) => {
+    prevVal += `--${key}: ${resultConfig.cssVars[key]};\n`;
+    return prevVal;
+  }, '');
+  app.head.style.push(
+    { innerHTML: `:root{\n ${stringStyle}}`, charset: 'utf-8' }
+  );
   inject('themeConfig', resultConfig);
 };
